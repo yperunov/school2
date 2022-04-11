eb49643c05089b672980f7f29e7ba4f3680dcec0
yperunov <perunovyuri@gmail.com>
2022-04-11 22:55:49 +0300

    Initial Commit

---
+++ src/main/java/ru/hogwarts/school2/School2Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(School2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/school2/School2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
